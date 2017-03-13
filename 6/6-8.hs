322cb1c4357e6d4680491e41068b2f104f948a8e
d.makarov <d.makarov@favbet.com>
2017-03-13 22:01:42 +0200

    solve 6.4

--- 6/6-8.hs
+++ 6/6-8.hs
@@ -9,7 +9,14 @@ sumdown 0 = 0
 sumdown n = n + sumdown(n-1)
 
 --3
+-- (^) :: Int -> Int -> Int
+-- m ^ 0 = 1
+-- m ^ n = m * (m ^ (n-1))
+
+--4
 euclid :: Int -> Int -> Int
-euclid a b | a == b = a
-           | otherwise euclid x y
-                where x = max
+euclid a b | a == b    = a
+           | otherwise = euclid x y
+                where
+                    x = abs (a - b)
+                    y = min a b
